16b3e6ce13850ced9be41a1c864bb239bc47a4b4
BuninaVeronika <Ferenicavv@gmail.com>
2021-05-06 23:56:47 +0300

    Редаирект нерабочий, и иди неправильный и вообще все, перепроврерить и в ходные даннные и запросы

--- php_form/passing_quest_tour.php
+++ php_form/passing_quest_tour.php
@@ -11,17 +11,57 @@ if (!empty($email)) {
 } elseif (!empty($cooki_hash_s)) {
     $s_mail = mysqli_query($connect, "SELECT `id_t` FROM `user_tourist` WHERE `cookies_hash`='$cooki_hash_s'");
 } else {
-    exit("Авторизуйтесь или Зарегистрируйтесь на сайте, чтобы иметь возможность походить квесты.");
+    exit("Авторизуйтесь или Зарегистрируйтесь на сайте, чтобы иметь возможность проходить квесты.");
 }
 $arys = mysqli_fetch_assoc($s_mail);
 $id_t = $arys["id_t"];
 
 $passing = mysqli_query($connect, "SELECT * FROM `passing` WHERE `id_t`='$id_t' AND `id_quests`='$id_quest' ORDER BY id DESC LIMIT 1");
 if (mysqli_num_rows($passing) > 0) {
-    //сделать запрос на следующее задание текущего квеста и на него переадресовать.
+    $passing_array = mysqli_fetch_assoc($passing);
+    $id_task_passing = $passing_array['id_task_passing'];
+    $id_quests = $passing_array['id_quests'];
+
+    $passing_arr = mysqli_query($connect, "SELECT `id_task_passing`, `id_task` FROM `task` WHERE `id_task_passing`='$id_task_passing'");
+
+    for ($i = 0; $i < mysqli_num_rows($passing_arr); $i++) {
+        $passing_result = mysqli_fetch_assoc($passing_arr);
+        $id_task_passing_res = $passing_result['id_task_passing'];
+        $id_task_res = $passing_result['id_task'];
+        if ($id_task_passing === $id_task_passing_res) {
+            $result_task = true;
+            continue;
+        } elseif ($result_task === true) {
+            $url_result = url_redirect($id_task_res);
+            $url_result .= '.php?$id_task_passing=' . $id_task_passing_res;
+            echo $url_result;
+            break;
+        } else {
+            continue;
+        }
+    }
+
 } else {
-    //поучаем запрос на первое задание
     $passing = mysqli_query($connect, "SELECT * FROM `task` WHERE `id_quests`='$id_quest' LIMIT 1");
-    //получаем иди квеста и в соответсвии с id переадресуем на страницу с передачей id_task_passing
+
+    $passing_result = mysqli_fetch_assoc($passing);
+    $id_task_passing_now = $passing_result['id_task_passing'];
+    $id_task_now = $passing_result['id_task'];
+
+    $url_result = url_redirect($id_task_now);
+    $url_result .= '.php?$id_task_passing=' . $id_task_passing_now;
+    echo $url_result;
+
 }
-echo('../passing/photo.php');
+function url_redirect($id_task)
+{
+    $url[] = ['photo', 'text_recognition', 'face_recognition', 'geodata', 'image_comparison', 'qr_photo', 'voice_recognition', 'text'];
+    $i = 1;
+    foreach ($url as $str) {
+        if ($i === $id_task) {
+            return $str;
+            break;
+        }
+        $i++;
+    }
+}
